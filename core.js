7c908ce064a5535531e04da0abf949aaa9ab081e
iki1uc <entertrainer@web.de>
2026-07-04 19:57:32 +0200

    Update core.js

--- core.js
+++ core.js
@@ -1,1 +1,45 @@
+// Trinity-Core + NameLage
+export const TrinityCore = {
+  piOP: {
+    flags: {},
+    types: {},
+    ops: {},
+    bind(param, value) {
+      this.flags[param] = value;
+    }
+  },
+
+  PRM: {
+    params: {},
+    set(key, val) {
+      this.params[key] = val;
+    },
+    get(key) {
+      return this.params[key];
+    }
+  },
+
+  modi: {
+    fn: {},
+    add(name, handler) {
+      this.fn[name] = handler;
+    },
+    run(name, input) {
+      return this.fn[name]?.(input);
+    }
+  },
+
+  // NEW: NAME-LAGE MODULE
+  nameLage: {
+    name: "Trinity",
+    lage: {
+      micro: "piOP",
+      parameter: "PRM (-2-)",
+      funktion: "modi (yziu)",
+      meta: "FLR"
+    },
+    stabilitaet: 0.95,
+    status: "NC-Startbereit"
+  }
+};
 
